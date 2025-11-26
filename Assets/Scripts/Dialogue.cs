aaf0a7af46040b1736c2443acf61288098a45004
SofiyaWalko <svalko940@gmail.com>
2025-11-27 00:57:09 +0300

    delete custom dialogue system

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -5,12 +5,5 @@ public class Dialogue
 {
     public string dialogueID;
     public string characterName;
-
-    [TextArea(3, 10)]
-    public string[] sentences;
-
-    [TextArea(3, 10)]
-    public string[] altSentences;
-
     public int requiredStatCount = 0;
 }
